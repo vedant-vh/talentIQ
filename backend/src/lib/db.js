43fd78c6609804224644b45440c6edfebb2db2f1
vedant-vh <vedanthadap123@gmail.com>
2026-02-09 18:58:40 +0530

    db and tools setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+import { ENV } from './env.js'
+
+export const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(ENV.DB_URL)
+        console.log("Connected to mongodb: ", conn.connection.host)
+    } catch (error) {
+        console.error("Error connecting mongodb")
+        process.exit(1) // 0 means success
+    }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { ENV } from './lib/env.js';
 import path from 'path'
+import { connectDB } from './lib/db.js';
 
 
 const app = express();
@@ -20,4 +21,15 @@ if (ENV.NODE_ENV === "production"){
     });
 }
 
-app.listen(ENV.PORT, () => console.log(`server running on port ${ENV.PORT}`))
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => console.log(`server running on port ${ENV.PORT}`));
+    } catch (error) {
+        console.log("errno starting the server",error)
+    }
+}
+
+startServer();
