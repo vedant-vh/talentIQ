a6c7f22105cf979319cbba3c457c448d42dfb915
vedant-vh <vedanthadap123@gmail.com>
2026-02-12 21:52:23 +0530

    auth and db sync

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,39 @@
+import {Inngest} from 'inngest'
+import { connectDB } from './db.js'
+import User from '../models/User.js'
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "talentIQ" });
+
+const syncUser = inngest.createFunction(
+    {id: "sync-user"},
+    {event: "clerk/user.created"},
+    async ({event}) => {
+        await connectDB()
+
+        const {id, email_addresses, first_name, last_name, image_url} = event.data
+
+        const newUser = {
+            clerkId: id,
+            email: email_addresses[0]?.email_address,
+            name: `${first_name || ""} ${last_name || ""}`,
+            profileImge: image_url
+        }
+
+        await User.create(newUser)
+    }
+)
+
+const deleteUserFromDB = inngest.createFunction(
+    {id: "delete-ser-from-db"},
+    {event: "clerk/user.deleted"},
+    async ({event}) => {
+        await connectDB()
+
+        const {id} = event.data
+
+        await User.deleteOne({clerkId: id});
+    }
+)
+
+export const functions = {syncUser, deleteUserFromDB}
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,27 @@ import express from 'express'
 import { ENV } from './lib/env.js';
 import path from 'path'
 import { connectDB } from './lib/db.js';
-
+import cors from 'cors'
+import {serve} from "inngest/express"
+import {inngest} from "./lib/inngest.js"
+import { functions } from './lib/inngest.js';
 
 const app = express();
 
-const __dirname = path.resolve()
+// const __dirname = path.resolve()
+
+//middleware
+app.use(express.json())
+//credentails:true means server allows brrowser to include cookies on request
+app.use(cors({origin: ENV.CLIENT_URL, credentials:true}))
+
+app.use("/api/inngest", serve({client: inngest, functions}))
 
 app.get("/qwe", (req,res) => {
     res.status(200).json("success from backend api")
 })
 
-//make app ready for deployment
+// make app ready for deployment
 if (ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
@@ -33,3 +43,5 @@ const startServer = async () => {
 }
 
 startServer();
+
+// export default app;
