fd0bf553b2daae73569cd6e43c54fa59cbc3db42
vedant-vh <vedanthadap123@gmail.com>
2026-02-14 14:04:05 +0530

    more corrections

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
-import { deleteStreamUser, upsertStreamUser } from "./stream.js";
+//import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "talent-iq" });
 
