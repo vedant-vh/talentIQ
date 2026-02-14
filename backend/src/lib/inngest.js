5888be55d8d0111941b39eb104aaf841aea5e5d4
vedant-vh <vedanthadap123@gmail.com>
2026-02-14 14:17:31 +0530

    little more corrections

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -35,6 +35,7 @@ const syncUser = inngest.createFunction(
 );
 
 
+
 const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },  //clerk/user.deleted
