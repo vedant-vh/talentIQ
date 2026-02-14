2bbe68cf283bc98ff49b4c59388f36857cc23665
vedant-vh <vedanthadap123@gmail.com>
2026-02-14 15:08:12 +0530

    revived stream to test

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,32 +1,32 @@
-// import { StreamChat } from "stream-chat";
-// import { ENV } from "./env.js";
+import { StreamChat } from "stream-chat";
+import { ENV } from "./env.js";
 
-// const apiKey = ENV.STREAM_API_KEY;
-// const apiSecret = ENV.STREAM_API_SECRET;
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
 
-// if (!apiKey || !apiSecret) {
-//   throw new Error("STREAM_API_KEY or STREAM_API_SECRET is missing");
-// }
+if (!apiKey || !apiSecret) {
+  throw new Error("STREAM_API_KEY or STREAM_API_SECRET is missing");
+}
 
-// export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
 
-// // create or update
-// export const upsertStreamUser = async (userData) => {
-//   try {
-//     await chatClient.upsertUser(userData);
-//     console.log("Stream user upserted successfully:", userData.id);
-//   } catch (error) {
-//     console.error("Error upserting stream user:", error);
-//     throw error;
-//   }
-// };
+// create or update
+export const upsertStreamUser = async (userData) => {
+  try {
+    await chatClient.upsertUser(userData);
+    console.log("Stream user upserted successfully:", userData.id);
+  } catch (error) {
+    console.error("Error upserting stream user:", error);
+    throw error;
+  }
+};
 
-// export const deleteStreamUser = async (userId) => {
-//   try {
-//     await chatClient.deleteUser(userId);
-//     console.log("Stream user deleted successfully:", userId);
-//   } catch (error) {
-//     console.error("Error deleting stream user:", error);
-//     throw error;
-//   }
-// };
+export const deleteStreamUser = async (userId) => {
+  try {
+    await chatClient.deleteUser(userId);
+    console.log("Stream user deleted successfully:", userId);
+  } catch (error) {
+    console.error("Error deleting stream user:", error);
+    throw error;
+  }
+};
